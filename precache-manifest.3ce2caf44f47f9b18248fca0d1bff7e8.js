ef033d86396d029f709bcf5b0769da2e9f6a48a3
CrioDo <rahul@criodo.com>
2020-09-28 15:09:38 +0530

    Updates

---
+++ precache-manifest.3ce2caf44f47f9b18248fca0d1bff7e8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c158e96fc9f44de7559ebc57475b8c7",
+    "url": "/Crio-Accelerate-May-2020-kavi6302/index.html"
+  },
+  {
+    "revision": "f61e5a315ea376572ba5",
+    "url": "/Crio-Accelerate-May-2020-kavi6302/static/css/2.57ddf7ad.chunk.css"
+  },
+  {
+    "revision": "20fda1114e87fd26d7aa",
+    "url": "/Crio-Accelerate-May-2020-kavi6302/static/css/main.5db1380d.chunk.css"
+  },
+  {
+    "revision": "f61e5a315ea376572ba5",
+    "url": "/Crio-Accelerate-May-2020-kavi6302/static/js/2.167256a3.chunk.js"
+  },
+  {
+    "revision": "d466ab9648f013dea081956fc7c904f3",
+    "url": "/Crio-Accelerate-May-2020-kavi6302/static/js/2.167256a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20fda1114e87fd26d7aa",
+    "url": "/Crio-Accelerate-May-2020-kavi6302/static/js/main.69c50fa3.chunk.js"
+  },
+  {
+    "revision": "6d37a827afaa35a793eb",
+    "url": "/Crio-Accelerate-May-2020-kavi6302/static/js/runtime-main.079d8f07.js"
+  },
+  {
+    "revision": "c9536e0497f939475da3110091e1a10d",
+    "url": "/Crio-Accelerate-May-2020-kavi6302/static/media/crio-logo-white.c9536e04.svg"
+  },
+  {
+    "revision": "63be71d01edef2dcf3cafba5e52553a4",
+    "url": "/Crio-Accelerate-May-2020-kavi6302/static/media/crio-logo.63be71d0.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Crio-Accelerate-May-2020-kavi6302/precache-manifest.3ce2caf44f47f9b18248fca0d1bff7e8.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Crio-Accelerate-May-2020-kavi6302/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
